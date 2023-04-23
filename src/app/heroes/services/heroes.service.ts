d7e512fae5f19ef7c99582061a9fbae83e332c5d
florius88 <florsantosbueno@gmail.com>
2023-04-23 13:29:41 +0200

    CRUD Endpoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
-import { Hero } from '../interfaces/hero.interface';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
+
+import { Hero } from '../interfaces/hero.interface';
 
 
 @Injectable({ providedIn: 'root' })
@@ -25,6 +26,24 @@ export class HeroesService {
 
     getSuggestions(query: string): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+    }
+
+    // CRUD Endpoints
+    addHero(hero: Hero): Observable<Hero> {
+        return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+    }
+
+    updateHero(hero: Hero): Observable<Hero> {
+        if (!hero.id) throw Error('Hero id is required');
+        return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHeroBtId(id: string): Observable<boolean> {
+        return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                catchError(err => of(false)),
+                map(resp => true)
+            );
     }
 
 }
