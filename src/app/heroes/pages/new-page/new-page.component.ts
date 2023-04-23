cde833bbd2de86a57d55715c1c929f6aeb29f7c3
florius88 <florsantosbueno@gmail.com>
2023-04-23 16:46:06 +0200

    Snackbar y redireccion

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { switchMap } from 'rxjs';
+
+import { Publisher, Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+
 
 
 @Component({
@@ -9,16 +16,16 @@ import { Publisher } from '../../interfaces/hero.interface';
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
-    id:               new FormControl<string>(''),
-    superhero:        new FormControl<string>('', { nonNullable: true }),
-    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
-    alter_ego:        new FormControl<string>(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
     first_appearance: new FormControl<string>(''),
-    characters:       new FormControl<string>(''),
-    alt_img:          new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>(''),
   })
 
   public publishers = [
@@ -26,11 +33,55 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ]
 
+  // CONSTRUCTOR
+  constructor(
+    private heroService: HeroesService,
+    private activaterRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+
+    this.activaterRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroService.getHeroById(id)),
+      )
+      .subscribe(hero => {
+        if (!hero) return this.router.navigateByUrl('/');
+        this.heroForm.reset(hero);
+        return;
+      })
+  }
+
+  // METODOS
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
   onSubmit() {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.getRawValue()
-    })
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroService.updateHero(this.currentHero)
+        .subscribe(hero => {
+          this.showSnackbar(`${hero.superhero} created`);
+        });
+
+      return;
+    }
+
+    this.heroService.addHero(this.currentHero)
+      .subscribe(hero => {
+        this.router.navigate(['/heroes/edit', hero.id]);
+        this.showSnackbar(`${hero.superhero} created`);
+      });
+  }
+
+  showSnackbar(message: string): void {
+    this.snackbar.open(message, 'DONE', { duration: 2500 })
   }
 
 }
