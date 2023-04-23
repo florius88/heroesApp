eb0a0aaa9b5001a0b193b2fe35becfd35931c11e
florius88 <florsantosbueno@gmail.com>
2023-04-23 13:53:56 +0200

    HeroForm - ReactiveForms

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
+
 
 @Component({
   selector: 'app-new-page',
@@ -8,9 +11,26 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', { nonNullable: true }),
+    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:        new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters:       new FormControl<string>(''),
+    alt_img:          new FormControl<string>(''),
+  })
+
   public publishers = [
-    { id: 'DC Comics', desc: 'DC - Comics'},
-    { id: 'Marvel Comics', desc: 'Marvel - Comics'},
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ]
+
+  onSubmit() {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.getRawValue()
+    })
+  }
 
 }
