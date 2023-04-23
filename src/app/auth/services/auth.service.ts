5ffd4f7624c66f725e9abe89bbd4da6e3dcef127
florius88 <florsantosbueno@gmail.com>
2023-04-23 20:19:51 +0200

    Mantener la sesion del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
@@ -36,6 +36,20 @@ export class AuthService {
     logout() {
         this.user = undefined;
         localStorage.clear();
+    }
+
+    checkAuthentication(): Observable<boolean> {
+        if (!localStorage.getItem('token')) return of(false)
+
+        const token = localStorage.getItem('token');
+        return this.http.get<User>(`${this.baseUrl}/users/1`)
+            .pipe(
+                tap(user => this.user = user),
+                /* Si el usuario existe, hacemos la doble negación para 
+                asegurarnos que nos devuelve un valor boolean */
+                map(user => !!user),
+                catchError(err => of(false))
+            );
     }
 
 }
